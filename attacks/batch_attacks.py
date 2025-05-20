f406c3e59c97b6192fffa3e05d5f467461ccc363
EliasOGP <elias.collaert@gmail.com>
2025-05-20 10:28:40 +0200

    updated license for torchattacks

--- attacks/batch_attacks.py
+++ attacks/batch_attacks.py
@@ -3,31 +3,13 @@ import torch
 import torch.nn as nn
 from torchattacks.attack import Attack
 
+# Modified code from https://github.com/Harry24k/adversarial-attacks-pytorch 
+# Authored by Harry Kim under an MIT License
+# The main modification is to make it work with varying attack parameters at inference time
+# Check out our paper for more info on the modifications
+
 
 class PGD(Attack):
-    r"""
-    PGD in the paper 'Towards Deep Learning Models Resistant to Adversarial Attacks'
-    [https://arxiv.org/abs/1706.06083]
-
-    Distance Measure : Linf
-
-    Arguments:
-        model (nn.Module): model to attack.
-        eps (float): maximum perturbation. (Default: 8/255)
-        alpha (float): step size. (Default: 2/255)
-        steps (int): number of steps. (Default: 10)
-        random_start (bool): using random initialization of delta. (Default: True)
-
-    Shape:
-        - images: :math:`(N, C, H, W)` where `N = number of batches`, `C = number of channels`,        `H = height` and `W = width`. It must have a range [0, 1].
-        - labels: :math:`(N)` where each value :math:`y_i` is :math:`0 \leq y_i \leq` `number of labels`.
-        - output: :math:`(N, C, H, W)`.
-
-    Examples::
-        >>> attack = torchattacks.PGD(model, eps=8/255, alpha=1/255, steps=10, random_start=True)
-        >>> adv_images = attack(images, labels)
-
-    """
 
     def __init__(self, model, eps=8 / 255, alpha=2 / 255, steps=10, random_start=True):
         super().__init__("PGD", model)
